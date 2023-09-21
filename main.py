5792d7dedf50fd772ffcd4135a589b75339b99f1
Anurag sarkar <anuragsarkar@Anurags-MacBook-Air.local>
2023-09-21 21:05:27 +0530

    ai shit

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import wx
 import cv2
+from transformers import AutoProcessor, AutoModelForZeroShotImageClassification
+from transformers import pipeline
+import torch
+import PIL.Image
+
+checkpoint = "openai/clip-vit-large-patch14"
+detector = pipeline(model=checkpoint, task="zero-shot-image-classification")
+model = AutoModelForZeroShotImageClassification.from_pretrained(checkpoint)
+processor = AutoProcessor.from_pretrained(checkpoint)
+
+
+def wx2PIL(bitmap):
+    size = tuple(bitmap.GetSize())
+    try:
+        buf = size[0]*size[1]*3*"\x00"
+        bitmap.CopyToBuffer(buf)
+    except:
+        del buf
+        buf = bitmap.ConvertToImage().GetData()
+    return PIL.Image.frombuffer("RGB", size, buf, "raw", "RGB", 0, 1)
 
 
 class viewWindow(wx.Frame):
@@ -13,7 +33,6 @@ class viewWindow(wx.Frame):
         self.image = wx.EmptyImage(self.imgSizer[0], self.imgSizer[1])
         self.imageBit = wx.BitmapFromImage(self.image)
         self.staticBit = wx.StaticBitmap(self.pnl, wx.ID_ANY, self.imageBit)
-
         self.vbox.Add(self.staticBit)
 
         self.capture = cv2.VideoCapture(0)
@@ -38,6 +57,17 @@ class viewWindow(wx.Frame):
             self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
             self.bmp.CopyFromBuffer(self.frame)
             self.staticBit.SetBitmap(self.bmp)
+
+            candidate_labels = ["tree", "car", "bike", "cat"]
+            inputs = processor(images=wx2PIL(self.bmp), text=candidate_labels,
+                               return_tensors="pt", padding=True)
+            with torch.no_grad():
+                outputs = model(**inputs)
+            logits = outputs.logits_per_image[0]
+            probs = logits.softmax(dim=-1).numpy()
+            scores = probs.tolist()
+            print(scores)
+
             self.Refresh()
 
 
